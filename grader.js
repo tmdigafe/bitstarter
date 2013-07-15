c2dd4c2e4f2fa140faa5610377602e1f4fd204d8
Ubuntu <ubuntu@ip-10-164-71-86.ec2.internal>
2013-07-15 04:20:00 +0000

    Updated: grader.js

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util');
 var HTMLFILE_DEFAULT = "index.html";
+var URL_DEFAULT = "http://infinite-badlands-5939.herokuapp.com/"
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +38,9 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+
+var assertUrlExists = function(val){    return val.toString();};
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -55,20 +61,48 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkURL = function(url, checksfile) {
+    $ = cheerio.load(url);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'url to check', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
-    exports.checkHtmlFile = checkHtmlFile;
-}
+
+	if (program.url) {
+		rest.get(program.url).on('complete', function(result) {
+		if (result instanceof Error) {
+    			sys.puts('Error : ' + result.message);
+    			this.retry(3000); // retry in 3 sec
+  		} else {
+    			//sys.puts(result);
+			var checkJson = checkURL(result, program.checks)};
+			var outJson = JSON.stringify(checkJson, null, 4);
+			console.log(outJson);
+  		});
+	} else {
+		var checkJson = checkHtmlFile(program.file, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	}
+
+} 
+
